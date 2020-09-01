70f2ce9fc9502daf1bb3f24c7d17d08b9124c678
johndepatie <john.depatie1@gmail.com>
2020-08-31 21:54:52 -0600

    Updated styling

--- public/javascripts/sidebarToggle.js
+++ public/javascripts/sidebarToggle.js
@@ -4,18 +4,3 @@
       $("#wrapper").toggleClass("toggled");
     });
 </script>
-
-<script>
-  $(function() {
-  var selectedClass = "";
-  $(".filter").click(function(){
-  selectedClass = $(this).attr("data-rel");
-  $("#gallery").fadeTo(100, 0.1);
-  $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-  setTimeout(function() {
-  $("."+selectedClass).fadeIn().addClass('animation');
-  $("#gallery").fadeTo(300, 1);
-  }, 300);
-  });
-  });
-</script>
