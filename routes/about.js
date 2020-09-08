f9647bfd429cfe76ca34f91711e09ab7cde3215f
johndepatie <john.depatie1@gmail.com>
2020-09-08 17:09:13 -0600

    Updated sidebar nav styles

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET about page. */
 router.get('/about', function(req, res) {
-  res.render('about', { title: 'About Us' });
+  res.render('about', { title: 'About Us', name: "about" });
 });
 
 module.exports = router;
