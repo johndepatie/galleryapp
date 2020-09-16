c7cefd96af9300d1f52889b42695132d2071ef33
johndepatie <john.depatie1@gmail.com>
2020-09-16 15:34:00 -0600

    Integrated database to gallery

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var Image = require('../models/imagegallery')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gallery', name: "projects" });
+  Image.find(function (err, imagegallery) {
+    if (err) console.log(err)
+
+    res.render('index', { title: 'Gallery', name: "projects", imagegallery: imagegallery, tag: "all"});
+  });
 });
 
 module.exports = router;
