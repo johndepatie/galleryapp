686499df35273acc2dfc12037b9afe32c04326de
johndepatie <john.depatie1@gmail.com>
2020-09-21 17:36:56 -0600

    Attempted to add tag count

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,42 @@ var express = require('express');
 var Image = require('../models/imagegallery')
 var router = express.Router();
 
+var count = [
+  Image.count(),
+  Image.countDocuments( {tags:"one"} ),
+  Image.find( {tags:"two"} ).size(),
+  Image.find( {tags:"three"} ),
+  Image.find( {tags:"test"} ),
+  Image.find( {tags:"a"} ),
+  Image.find( {tags:"b"} ),
+  Image.find( {tags:"c"} )
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Image.find(function (err, imagegallery) {
     if (err) console.log(err)
 
-    res.render('index', { title: 'Gallery', name: "projects", imagegallery: imagegallery, tag: "all"});
+    res.render('index', {
+      title: 'Gallery',
+      name: "projects",
+      imagegallery: imagegallery,
+      tag: "all",
+      count
+    });
   });
 });
 
 router.get('/tag=one', function(req, res, next) {
-  Image.find(function (err, imagegallery) {
+  Image.find( { tags:"one" }, function (err, imagegallery) {
     if (err) console.log(err)
-
-    res.render('index', { title: 'Gallery', name: "one", imagegallery: imagegallery, tag: "one"});
+    res.render('index', {
+      title: 'Gallery',
+      name: "one",
+      imagegallery: imagegallery,
+      tag: "one",
+      count
+    });
   });
 });
 
